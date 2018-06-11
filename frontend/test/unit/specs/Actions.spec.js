a74bea659c760722e7be41144f05fe823f3dd6e9
Margo <luckinamargo@gmail.com>
2018-06-11 17:07:23 +0700

    add action test

---
+++ frontend/test/unit/specs/Actions.spec.js
@@ -0,0 +1,28 @@
+import {listModule} from '../../../src/vuex/store';
+import Vuex from 'vuex';
+import Vue from 'vue';
+Vue.use(Vuex)
+const catsList = 'catsList'
+import _ from 'lodash';
+
+const commentsListStubbed = _.cloneDeep(listModule)
+
+const payload = {
+  'id': 0,
+  'alias': 'don_kihot',
+  'name': 'Дон',
+  'surname': 'Кихот',
+  'pics': 6
+}
+
+
+describe('actions', () => {
+  it('catsList', () => {
+    const stubbedStore = new Vuex.Store(commentsListStubbed);
+    stubbedStore.dispatch(catsList, payload);
+    console.log(commentsListStubbed.state.list)
+    expect(commentsListStubbed.state.list).to.deep.equal(payload)
+  })
+})
+
+
